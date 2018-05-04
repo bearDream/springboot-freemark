0997fc8bac7686dd2881fe540ab470eb2bae5857
bearDream <laxzhang@outlook.com>
2018-05-04 09:47:34 +0800

    first

---
+++ src/main/java/controller/HelloController.java
@@ -0,0 +1,43 @@
+package controller;
+
+import java.util.Map;
+
+
+
+import org.springframework.stereotype.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+ 
+
+/**
+
+ * 测试velocity;
+
+ * @author Angel --守护天使
+
+ * @version v.0.1
+
+ * @date 2016年10月4日
+
+ */
+
+@Controller
+
+public class HelloController {
+
+   
+
+    @RequestMapping("/hello")
+
+    public String hello(Map<String,Object> map){
+
+       map.put("name", "[Angel -- 守护天使]");
+
+       return "hello";
+
+    }
+
+   
+
+}
